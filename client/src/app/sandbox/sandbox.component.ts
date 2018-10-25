baa168c691dab363e4acbf4c5dacd38a9fcd1e0f
Howard Leung <hwrdleung@gmail.com>
2018-10-25 07:50:55 -1000

    Added resizer to sandbox component.  Works properly.

--- client/src/app/sandbox/sandbox.component.ts
+++ client/src/app/sandbox/sandbox.component.ts
@@ -8,12 +8,46 @@ import { DataService } from '../data.service';
 })
 export class SandboxComponent implements OnInit {
 
-  constructor(private data:DataService) { }
+  constructor(private data: DataService) { }
 
 
   ngOnInit() {
+    this.enableSandboxResizer();
   }
 
- 
+  enableSandboxResizer() {
+    let startResize = () => {
+      document.addEventListener('mousemove', this.resizeGrid);
+      document.addEventListener('mouseup', stopResize);
+    }
+
+    let stopResize = () => {
+      document.removeEventListener('mousemove', this.resizeGrid);
+      document.removeEventListener('mouseup', stopResize);
+    }
+
+    let resizer = document.getElementById('sandbox-resizer');
+    if (resizer) {
+      resizer.addEventListener('mousedown', startResize);
+    }
+  }
+
+  resizeGrid = (e) => {
+    let sandboxContainer = document.getElementById('sandbox-container');
+    let resizer = document.getElementById('sandbox-resizer');
+    let middleBarHeight = document.getElementsByClassName('sandbox-middle-bar')[0].clientHeight;
+
+    let viewportHeight = document.documentElement.clientHeight;
+    let offset = viewportHeight - sandboxContainer.clientHeight;
+
+    let topHeight = e.pageY - offset - resizer.clientHeight / 2;
+    let bottomHeight = viewportHeight - e.pageY - resizer.clientHeight / 2;
+
+    if (e.pageY < viewportHeight - middleBarHeight - resizer.clientHeight / 2) {
+      sandboxContainer.style.gridTemplateRows = topHeight + 'px ' + resizer.clientHeight + 'px ' + bottomHeight + 'px ';
+    }
+  }
+
+
 
 }
