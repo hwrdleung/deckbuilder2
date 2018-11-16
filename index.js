00026800716cd46d84428e2cdd245f837b281fbe
Howard Leung <hwrdleung@gmail.com>
2018-11-15 21:22:47 -1000

    Debug and deploy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(user);
+app.use('/user', user);
 
 // Routes
 app.get('/', (req, res) => {
